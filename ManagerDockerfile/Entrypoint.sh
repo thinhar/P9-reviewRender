3cc1903105d9d5a41fb39a1eff316699a603ae84
martin karkov <thinhar2@gmail.com>
2019-10-15 13:16:43 +0200

    no need to clone and entry point added again

--- ManagerDockerfile/Entrypoint.sh
+++ ManagerDockerfile/Entrypoint.sh
@@ -1,7 +1,9 @@
 # make sure we have the up to date things from git, so we dont have to create a new dockerimage every time we make changes
 # since the dockerimage is static and the things on git hub is not. 
 git reset --hard
-git clone "https://github.com/thinhar/P9-reviewRender.git" && cd /home/P9-reviewRender && git checkout manger-Api
+
+# git clone "https://github.com/thinhar/P9-reviewRender.git" && cd /home/P9-reviewRender && 
+git checkout manger-Api
 
 #chmod +x /home/P9-reviewRender/nodejs/server.js
 cd /home/P9-reviewRender/nodejs
