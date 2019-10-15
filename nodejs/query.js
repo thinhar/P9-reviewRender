3732aa3682b119b84f5899f1c45a544b12590193
martin karkov <thinhar2@gmail.com>
2019-10-15 16:05:11 +0200

    file upload endpoint added- untested

--- nodejs/query.js
+++ nodejs/query.js
@@ -3,6 +3,23 @@ const router = express.Router();
 
 var exec = require('child_process').exec,child;
 
+app.post('/upload', function(req, res, next) {
+
+        if (err) {
+            res.status(400).json({
+                error: stderr
+            });
+        }
+        else {
+          output = console.log(req.files.foo.name); 
+            res.status(200).json({
+                message:        "Result from backend",
+                output:         output
+            });
+        }
+  //console.log(req.files.foo.name); // the uploaded file object
+});
+
 router.get('/', (req, res, next) => {
     exec('date', function(err, stdout, stderr) {
        	if(err) {
