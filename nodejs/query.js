3ec7c25c466524ee6a6544e28ebabd0d9af09b27
martin karkov <thinhar2@gmail.com>
2019-10-15 18:17:17 +0200

    scheme

--- nodejs/query.js
+++ nodejs/query.js
@@ -8,7 +8,7 @@ app.use(fileUpload());
 var exec = require('child_process').exec,child;
 
 router.post('/upload', function(req, res, next) {
-  echo !req.files 
+  echo (!req.files) 
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
