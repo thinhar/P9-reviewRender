eb8941696544defbcd3551980595ba72d2a46807
martin karkov <thinhar2@gmail.com>
2019-10-15 12:39:21 +0200

    initial entrypoint script added

---
+++ ManagerDockerfile/Entrypoint.sh
@@ -0,0 +1,4 @@
+# make sure we have the up to date things from git, so we dont have to create a new dockerimage every time we make changes
+# since the dockerimage is static and the things on git hub is not. 
+git clone "https://github.com/thinhar/P9-reviewRender.git" && cd /home/P9-reviewRender && git checkout manger-Api
+
