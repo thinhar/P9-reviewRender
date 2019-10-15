a2b47ed713f20533eb95918a4d52a8f59291d346
martin karkov <thinhar2@gmail.com>
2019-10-15 18:42:42 +0200

    how about if we have no error catch

--- nodejs/query.js
+++ nodejs/query.js
@@ -9,9 +9,9 @@ var exec = require('child_process').exec,child;
 
 router.post('/upload', function(req, res, next) {
   console.log(!req.files);
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
+  // if (!req.files || Object.keys(req.files).length === 0) {
+  //   return res.status(400).send('No files were uploaded.');
+  // }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.sampleFile;
