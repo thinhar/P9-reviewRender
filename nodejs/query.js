943ed4246d9cd4ba7c3c62523e03284d50856208
martin karkov <thinhar2@gmail.com>
2019-10-23 13:42:47 +0200

    postman failure halted development, but was found at this point

--- nodejs/query.js
+++ nodejs/query.js
@@ -12,7 +12,7 @@ router.post('/upload', async (req, res) => {
             });
         } else {
             //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let avatar = req.files.justStuff;
+            let avatar = req.files.avatar;
             
             //Use the mv() method to place the file in upload directory (i.e. "uploads")
             avatar.mv('./uploads/' + avatar.name);
