d0221fee276c53b87fe681fe95a2ed5e8d0defd5
martin karkov <thinhar2@gmail.com>
2019-11-07 17:37:24 +0100

    added prints to script

--- BlenderDockerfile/renderscript/renderscript.sh
+++ BlenderDockerfile/renderscript/renderscript.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+echo "start script"
 x2=$(/usr/bin/amqp-get --url=amqp://guest:guest@rabbitmq-service:5672 -q workqueue2)
 status=$?
-
+echo "status: $status"
 while [ $status -eq 0 ]
 do
 /usr/local/blender/blender -b -noaudio /home/shared/$x2 -o /home/shared/frame_### -f 1
 
-x2=$(/usr/bin/amqp-get --url=amqp://guest:guest@rabbitmq-service:5672 -q workqueue2)
+x2=$(/usr/bin/amqp-get --url=amqp://guest:guest@rabbitmq-service:5672 -q foo2)
 status=$?
 echo "status: $status"
 #  status=$(( $x + 2 ))
