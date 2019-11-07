506d2893fbaaae3a625ab7b25c8ca8a67b646cd0
martin karkov <thinhar2@gmail.com>
2019-11-07 17:07:29 +0100

    added script for blenderworkers and changed volume claim name

---
+++ BlenderDockerfile/renderscript/renderscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+x2=$(/usr/bin/amqp-get --url=amqp://guest:guest@rabbitmq-service:5672 -q workqueue2)
+status=$?
+
+while [ $status -eq 0 ]
+do
+/usr/local/blender/blender -b -noaudio /home/shared/$x2 -o /home/shared/frame_### -f 1
+
+x2=$(/usr/bin/amqp-get --url=amqp://guest:guest@rabbitmq-service:5672 -q workqueue2)
+status=$?
+echo "status: $status"
+#  status=$(( $x + 2 ))
+done
