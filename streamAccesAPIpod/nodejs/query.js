0be33c8d6de50125f17e0057fbc81904f02a4e8f
martin karkov <thinhar2@gmail.com>
2019-12-09 21:16:53 +0100

    streamaccesAPI: changes to a endpoint

--- streamAccesAPIpod/nodejs/query.js
+++ streamAccesAPIpod/nodejs/query.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 var exec = require('child_process').exec,child;
+var fs = require('fs');
 
 //router.get('/:URL/:UA/:DELETECOOKIES', (req, res, next) => {
 //exec(`./P7-DimensionalShopping/Backend/callQueryWithUser.sh ${curURL} ${curUA} ${curDELETECOOKIES}`, function(err, stdout, stderr) {
@@ -28,18 +29,19 @@ router.get('/stream/:STREAMNAME', (req, res, next) => {
   const streamName = req.params.STREAMNAME;
 
   console.log(streamName);
-  htmlReturnText= '<html> <head> <meta name="viewport" content="width=device-width"> </head><body><video controls name="media"><source src="/home/shared/'+streamName+'/output.webm" type="video/webm">Your browser doesnt support HTML5 video tag.</video></body></html>';
+  htmlReturnText= '<html> <head> <meta name="viewport" content="width=device-width"> </head><body><video controls name="media"><source src="40.88.23.193:80/query/stream/'+streamName+'/data" type="video/webm">Your browser doesnt support HTML5 video tag.</video></body></html>';
 
 
   res.status(200).send(htmlReturnText);
 
 });
+
 router.get('/stream/:STREAMNAME/data', (req, res, next) => {
 
   const streamName = req.params.STREAMNAME;
 
-  res.setHeader("content-type", "some/type");
-  fs.createReadStream("/home/shared/"+streamName+"output.webm").pipe(res);
+  res.setHeader("content-type", "video/webm");
+  fs.createReadStream("/home/shared/"+streamName+"/output.webm").pipe(res);
 
 });
 
