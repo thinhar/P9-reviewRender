a78f21523b0e255e30426e909af5ae67ef845f6e
thinhar <mkarko15@student.aau.dk>
2020-04-05 15:34:08 +0200

    file name missmatch fixed

--- BlenderDockerfile/renderscript/startpythonrender.sh
+++ BlenderDockerfile/renderscript/startpythonrender.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/analyser.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/pythonRender.py
 status=$?
 while [ $status -gt 0 ]
 do
-/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/analyser.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/pythonRender.py
 status=$?
 done
 
