0fbb2ed11a25cd5d7261b90da36ec8e7f414e27b
thinhar <mkarko15@student.aau.dk>
2020-04-05 16:35:47 +0200

    path discrepancy fix

--- BlenderDockerfile/renderscript/startpythonrender.sh
+++ BlenderDockerfile/renderscript/startpythonrender.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/pythonRender.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/renderscript/pythonRender.py
 status=$?
 while [ $status -gt 0 ]
 do
-/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/pythonRender.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/renderscript/pythonRender.py
 status=$?
 done
 read -p " get user input" name
