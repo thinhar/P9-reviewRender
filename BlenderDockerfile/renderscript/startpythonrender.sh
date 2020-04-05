4c2ab5b69b08d57e9e36afea624efa898aa4dbd0
thinhar <mkarko15@student.aau.dk>
2020-04-05 17:01:34 +0200

    more dictory stuff

--- BlenderDockerfile/renderscript/startpythonrender.sh
+++ BlenderDockerfile/renderscript/startpythonrender.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/renderscript/pythonRender.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/script/pythonRender.py
 status=$?
 while [ $status -gt 0 ]
 do
-/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/renderscript/pythonRender.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/script/pythonRender.py
 status=$?
 done
 read -p " get user input" name
