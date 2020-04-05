a6d8a53e8f4754e5c142921e6554dcb5c7a76776
thinhar <mkarko15@student.aau.dk>
2020-04-05 15:20:16 +0200

    wrong method of sending variable used, now changed to right one

--- BlenderDockerfile/renderscript/startpythonrender.sh
+++ BlenderDockerfile/renderscript/startpythonrender.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/usr/local/blender/blender -b -noaudio /home/shared/$incomingname --python /home/nodejs/analyser.py
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/analyser.py
 status=$?
 while [ $status -gt 0 ]
 do
-    /usr/local/blender/blender -b -noaudio /home/shared/$incomingname --python /home/nodejs/analyser.py
-    status=$?
+/usr/local/blender/blender -b -noaudio /home/shared/$QUEUE.blend --python /home/nodejs/analyser.py
+status=$?
 done
 
 exit $status
