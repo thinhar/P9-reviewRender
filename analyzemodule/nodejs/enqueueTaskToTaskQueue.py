14fffb736b032f8b994463ca92e95945496e1acb
Kim <kimhtxt12@gmail.com>
2020-04-09 12:04:52 +0200

    update

--- analyzemodule/nodejs/enqueueTaskToTaskQueue.py
+++ analyzemodule/nodejs/enqueueTaskToTaskQueue.py
@@ -3,16 +3,15 @@ import pika
 import os
 import sys
 
-nameOfTask = sys.argv[1];
-numberOfpodsRequired = int(sys.argv[2]);
-ResourceRequirements = sys.argv[3];
+nameOfTask = sys.argv[1]
+numberOfpodsRequired = int(sys.argv[2])
+resourceRequirements = sys.argv[3]
 
 print (nameOfTask) 
 print (numberOfpodsRequired) 
-print (ResourceRequirements)
+print (resourceRequirements)
 
 rabbit = os.getenv('RABBITMQ_SERVICE_PORT_5672_TCP_ADDR')
-
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit))
@@ -22,11 +21,11 @@ channel = connection.channel()
 queueName= 'taskQueue'
 i = 1
 while i <= numberOfpodsRequired:
-	i += 1;
+	i += 1
 	channel.basic_publish(
 		exchange='',
 		routing_key=queueName,
-		body=nameOfTask+' '+ ResourceRequirements,
+		body=nameOfTask+' '+ resourceRequirements,
 		properties=pika.BasicProperties(
 			delivery_mode=2,  # make message persistent
 			priority=1 #1 is lowers priority
